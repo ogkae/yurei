cb1a4325711873b563118f4604a0057e60024ef1
Kae. (⿻) <140287817+ogkae@users.noreply.github.com>
2025-11-11 18:51:09 +0100

    V1.3.1
    [A/+] Patch

--- yurei/__init__.py
+++ yurei/__init__.py
@@ -1,15 +1,32 @@
-from .cipher import encrypt_bytes, decrypt_bytes, encrypt_parallel, decrypt_parallel
-from .uid import uuid4, is_uuid4, sha256_id, short_id
-from .obfusc import xor_obfuscate, xor_deobfuscate
+""" identifiers, authentication, session,
+    encryption, obfuscation and storage."""
+
+from .uid import is_uuid4, sha256_id, short_id, uuid4
+from .obfusc import xor_deobfuscate, xor_obfuscate
 from .auth import hash_password, verify_password
 from .session import create_token, verify_token
 from .store import KVStore
 
-__all__ = [
-    "uuid4","is_uuid4","sha256_id","short_id",
-    "hash_password","verify_password",
-    "create_token","verify_token",
-    "encrypt_bytes","decrypt_bytes","encrypt_parallel","decrypt_parallel",
+from .cipher import (
+    decrypt_bytes,
+    decrypt_parallel,
+    encrypt_bytes,
+    encrypt_parallel,
+)
+__all__ = (
+    "uuid4",
+    "is_uuid4",
+    "sha256_id",
+    "short_id",
+    "hash_password",
+    "verify_password",
+    "create_token",
+    "verify_token",
+    "encrypt_bytes",
+    "decrypt_bytes",
+    "encrypt_parallel",
+    "decrypt_parallel",
     "KVStore",
-    "xor_obfuscate","xor_deobfuscate",
-]
+    "xor_obfuscate",
+    "xor_deobfuscate",
+)
